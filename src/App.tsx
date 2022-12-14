ac2a57c348d5f6c10d80898f07528f2a890f2184
Daniel Beff <daniel.beff@gmail.com>
2022-12-14 02:45:12 +0100

    Initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./assets/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
+    <div className="min-h-screen md:container md:mx-auto">
+      <header className="flex align-middle justify-between">
+        <a href="/" className="block p-5">
+          <img src={logo} className="" alt="logo" />
+        </a>
+        <div className="flex">
+          <a href="/" className="block p-5 text-white ">
+            Main
           </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
+          <a href="/" className="block p-5 text-white ">
+            Login
           </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        </div>
       </header>
+      <section className="grid place-content-center">
+        <div className="bg-white rounded-md shadow-md min-h-[200px] min-w-[500px] p-8">
+          <div className="py-2">
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="block w-full p-2.5 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-600 focus:border-violet-500 focus-visible:outline-violet-500 appearance-none"
+              required
+            />
+          </div>
+          <div className="py-2">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Password
+            </label>
+            <input
+              type="text"
+              id="password"
+              className="block w-full p-2.5 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-600 focus:border-violet-500 focus-visible:outline-violet-500 appearance-none"
+              required
+            />
+          </div>
+          <div className="py-2 grid place-content-center ">
+            <button
+              type="submit"
+              className="text-white bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-full text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
