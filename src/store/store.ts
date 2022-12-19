c50260304d458aeb210ba7c6cb185f25b23f2388
Daniel Beff <daniel.beff@gmail.com>
2022-12-19 12:29:54 +0100

    Fixed issue with redux persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -14,6 +14,7 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
   key: "root",
   storage,
+  whitelist: ["auth"],
 };
 
 const rootReducer = combineReducers({
@@ -25,6 +26,10 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export const persistor = persistStore(store);
